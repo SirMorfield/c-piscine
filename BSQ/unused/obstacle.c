29da87e5bc2ad6bd695e75a5fa9d67c20108e32f
SirMorfield <joppe.koers@gmail.com>
2020-08-13 22:06:40 +0200

    a

--- BSQ/unused/obstacle.c
+++ BSQ/unused/obstacle.c
@@ -1,87 +1,89 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   obstacle.c                                         :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: joppe <joppe@student.codam.nl>               +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2020/07/27 22:37:44 by joppe         #+#    #+#                 */
-/*   Updated: 2020/07/28 22:46:27 by joppe         ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "canvas.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint-gcc.h>
+#include <string.h>
+#include "../helpers/ft_str.h"
 
-uint16_t	n_occurrences(char *str, uint16_t strlen, char c)
+typedef struct	s_node {
+    char		*buf;
+    struct		s_node *next;
+}				t_node;
+
+t_node *malloc_node(int32_t fd, uint64_t block_size, uint64_t *total_size)
 {
-	uint16_t occurrences;
+	t_node		*node;
+	int32_t		bytes_read;
 
-	occurrences = 0;
-	while (strlen > 0)
+	node = malloc(sizeof(t_node));
+	node->buf = malloc(block_size);
+	bytes_read = read(fd, node->buf, block_size);
+	*total_size += bytes_read;
+	if (bytes_read == block_size)
 	{
-		strlen--;
-		if(str[strlen] == c)
-			occurrences++;
+		node->next = malloc(sizeof(t_node));
 	}
-	return (occurrences);
+	else
+	{
+		node->buf[bytes_read] = '\0';
+		*total_size += 1;
+		node->next = NULL;
+	}
+	return (node);
 }
 
-void		set_obstacle_locations(t_wp *wp)
+t_node *get_list(char *filename, uint64_t *total_size, uint64_t block_size)
 {
-	uint16_t x;
-	uint16_t y;
-	uint16_t occurrences_i;
-	uint16_t occurrences;
+	int fd;
+	t_node *head;
+    t_node *current;
 
-	wp->obstacles = malloc(wp->y_size * sizeof(int16_t*));
-	y = 0;
-	while (y < wp->y_size)
+    fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+	head = malloc_node(fd, block_size, total_size);
+	current = head->next;
+    while (current != NULL)
 	{
-		occurrences = n_occurrences(wp->map[y], wp->x_size, wp->obstacle);
-		wp->obstacles[y] = malloc((occurrences + 1) * sizeof(int16_t));
-		wp->obstacles[y][occurrences] = -1;
-		occurrences_i = 0;
-		x = 0;
-		// printf(">");
-		while (x < wp->x_size)
-		{
-			if (wp->map[y][x] == wp->obstacle)
-			{
-				wp->obstacles[y][occurrences_i] = (int16_t)x;
-				// printf("%u ", x);
-				occurrences_i++;
-			}
-			x++;
-		}
-		// printf("\n");
-		y++;
+		current = malloc_node(fd, block_size, total_size);
+		current = current->next;
 	}
+    close(fd);
+	return (head);
 }
 
-int			is_valid_square2(t_wp *wp, uint16_t x, uint16_t y, uint16_t size)
+char *readfile(char *filename)
 {
-	uint16_t max_x;
-	uint16_t max_y;
-	uint16_t y_i;
-	uint16_t i;
+	const uint64_t	block_size = 10;
+	uint64_t 		total_size;
+	t_node			*list;
+	t_node			*next;
+	char			*res;
+	uint64_t		res_i;
 
-	max_x = (x + size) - 1;
-	max_y = y + size;
-	y_i = y;
-	while (y_i < max_y)
+	list = get_list(filename, &total_size, block_size);
+	res = malloc(total_size * sizeof(char));
+	res_i = 0;
+	while (list != NULL)
 	{
-		i = 0;
-		while (wp->obstacles[y_i][i] != -1)
-		{
-			if (wp->obstacles[y_i][i] > max_x)
-				break ;
-			if (wp->obstacles[y_i][i] >= x)
-				return (0);
-			i++;
-		}
-		y_i++;
+		ft_strncpy(res + res_i, list->buf, block_size);
+		res_i += block_size;
+		free(list->buf);
+		next = list->next;
+		free(list);
+		list = next;
 	}
-	return (1);
+	return (res);
+}
+
+int main(void) {
+
+	char *res;
+
+	res = readfile("test.txt");
+
+	printf("<%s>\n", res);
+	free(res);
+    return 0;
 }
